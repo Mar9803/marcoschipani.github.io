42b08f74a7a4b3d8f0e1a874b1ea6210891fd95f
MarcoSchipani <mschipani75@gmail.com>
2026-02-27 02:52:38 +0100

    🎨 fix: added base path for correct CSS loading

--- astro.config.ts
+++ astro.config.ts
@@ -13,7 +13,8 @@ import { SITE } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://marcoschipani.github.io",
+  site: "https://mar9803.github.io", 
+  base: "/marcoschipani.github.io",
   integrations: [
     sitemap({
       filter: page => SITE.showArchives || !page.endsWith("/archives"),
