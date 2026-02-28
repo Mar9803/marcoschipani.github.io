54a64f56cdea4f41e1bfdee48a87776deaeac840
MarcoSchipani <mschipani75@gmail.com>
2026-02-28 18:04:57 +0100

    aggiornamento 18.04

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig, envField, fontProviders } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
-import remarkMath from "remark-math"; 
-import rehypeKatex from "rehype-katex";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import {
@@ -23,14 +21,9 @@ export default defineConfig({
     }),
   ],
   markdown: {
-    // Nota: Ho aggiunto "as any" o rimosso commenti che possono infastidire il linter
-    remarkPlugins: [
-      remarkMath, 
-      remarkToc, 
-      [remarkCollapse, { test: "Table of contents" }]
-    ],
-    rehypePlugins: [rehypeKatex as any], 
+    remarkPlugins: [remarkToc, [remarkCollapse, { test: "Table of contents" }]],
     shikiConfig: {
+      // For more themes, visit https://shiki.style/themes
       themes: { light: "min-light", dark: "night-owl" },
       defaultColor: false,
       wrap: false,
@@ -43,6 +36,10 @@ export default defineConfig({
     },
   },
   vite: {
+    // eslint-disable-next-line
+    // @ts-ignore
+    // This will be fixed in Astro 6 with Vite 7 support
+    // See: https://github.com/withastro/astro/issues/14030
     plugins: [tailwindcss()],
     optimizeDeps: {
       exclude: ["@resvg/resvg-js"],
